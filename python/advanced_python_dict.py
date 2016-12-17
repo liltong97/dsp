89c0167c0ccc292ca406402c359c4f6cda76df5d
Lillian Tong <lillianjietong@gmail.com>
2016-12-17 11:10:40 -0800

    finished advanced python

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -11,7 +11,11 @@ def first_last_name_tuple_array(df):
   names = df['name']
   name_array = []
   for name in names:
-    split_names = name.split()
+    split_names_orig = name.split()
+    if len(split_names_orig[0]) < 3:
+      split_names = [split_names_orig[1], split_names_orig[0], split_names_orig[2]]
+    else:
+      split_names = split_names_orig
     name_array.append((split_names[-1], split_names[0]))
   return name_array
 
@@ -20,15 +24,37 @@ def faculty_dictionary(df):
   name_array = first_last_name_tuple_array(df)
   for line in df.itertuples(name = None):
     index = line[0]
-    if name_array[index][1] in faculty_dict.keys():
+    if name_array[index][0] in faculty_dict.keys():
+      faculty_dict[name_array[index][0]].append([line[2].strip(), line[3], line[4]])
     else:
-      faculty_dict[name_array[index[1]]] =
+      faculty_dict[name_array[index][0]] = [[line[2].strip(), line[3], line[4]]]
   return faculty_dict
 
+def better_faculty_dictionary(df):
+  better_faculty_dict = {}
+  name_array = first_last_name_tuple_array(df)
+  for line in df.itertuples(name = None):
+    index = line[0]
+    curr_name = name_array[index]
+    curr_name = curr_name[::-1]
+    if curr_name in better_faculty_dict.keys():
+      better_faculty_dict[curr_name].append([line[2].strip(), line[3], line[4]])
+    else:
+      better_faculty_dict[curr_name] = [line[2].strip(), line[3], line[4]]
+  return better_faculty_dict
+
+def print_by_last_name(better_faculty_dict):
+  keys = better_faculty_dict.keys()
+  keys.sort(key=lambda x: x[1])
+  for key in keys:
+    print str(key) + ': ' + str(better_faculty_dict[key])
 
 
 filepath = '/Users/lilliantong/ds/metis/metisgh/prework/dsp/python/faculty.csv'
 df = pd.read_csv(filepath)
-name_array =
+faculty_dict = faculty_dictionary(df)
+better_faculty_dict = better_faculty_dictionary(df)
+print_by_last_name(better_faculty_dict)
+
 
 
